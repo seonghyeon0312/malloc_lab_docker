ebab6ee4bf65a1b5d5f95ffd24f0ea8328139e0d
usingnameing <cnrrn0312@gmail.com>
2025-04-30 14:06:13 +0900

    Add: macro

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -42,8 +42,6 @@ team_t team = {
     /* Second member's email address (leave blank if none) */
     ""
 };
-    ""
-};
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
 
@@ -52,10 +50,6 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
-// Basic constants and macros
-#define WSIZE 4 // 워드 = 헤더 = 풋터 사이즈(bytes)
-#define DSIZE 8 // 더블워드 사이즈(bytes)
-#define CHUNKSIZE (1<<12) // heap을 이정도 늘린다(bytes)
 // Basic constants and macros
 #define WSIZE 4 // 워드 = 헤더 = 풋터 사이즈(bytes)
 #define DSIZE 8 // 더블워드 사이즈(bytes)
@@ -120,19 +114,8 @@ int mm_init(void)
 {   
     heap_listp = mem_sbrk(3*DSIZE);
     if (heap_listp == (void*)-1){
-{   
-    heap_listp = mem_sbrk(3*DSIZE);
-    if (heap_listp == (void*)-1){
         return -1;
     }
-    PUT(heap_listp, 0); //Unused padding
-    PUT(heap_listp + WSIZE, PACK(2*DSIZE,1)); 
-    PUT(heap_listp + 2*WSIZE,NULL); 
-    PUT(heap_listp + 3*WSIZE,NULL); 
-    PUT(heap_listp + 4*WSIZE,PACK(2*DSIZE,1)); 
-    PUT(heap_listp + 5*WSIZE,PACK(0,1)); 
-
-    free_listp = heap_listp + DSIZE;
     // last_bp = free_listp; // next_fit 을 위한 추가
     // Extend the empty heap with a free block of CHUNKSIZE bytes
     if (extend_heap(CHUNKSIZE/WSIZE) == NULL) 
@@ -144,8 +127,6 @@ int mm_init(void)
     PUT(heap_listp + 5*WSIZE,PACK(0,1)); 
 
     free_listp = heap_listp + DSIZE;
-    // last_bp = free_listp; // next_fit 을 위한 추가
-    // Extend the empty heap with a free block of CHUNKSIZE bytes
     if (extend_heap(CHUNKSIZE/WSIZE) == NULL) 
         return -1;
     return 0;
@@ -186,8 +167,6 @@ static void *coalesce(void *bp)
     putFreeBlock(bp); 
     return bp;
 }
-
-
 
 static void *extend_heap(size_t words)
 {
@@ -287,25 +266,9 @@ void *mm_malloc(size_t size)
     size_t asize; 
     size_t extendsize;
     void *bp; 
-
-   
-    if(size <= 0) 
-    void *bp; 
-
    
     if(size <= 0) 
         return NULL;
-    
-   
-    if(size <= DSIZE)
-        asize = 2*DSIZE; 
-    else         
-        asize = DSIZE * ((size+(DSIZE)+(DSIZE-1)) / DSIZE);
-
-   
-    if((bp = find_fit(asize))!=NULL){
-        place(bp,asize); 
-    
    
     if(size <= DSIZE)
         asize = 2*DSIZE; 
@@ -317,11 +280,6 @@ void *mm_malloc(size_t size)
         place(bp,asize); 
         return bp;
     }
-
-  
-    extendsize = MAX(asize,CHUNKSIZE);
-    if((bp = extend_heap(extendsize/WSIZE)) == NULL)
-  
     extendsize = MAX(asize,CHUNKSIZE);
     if((bp = extend_heap(extendsize/WSIZE)) == NULL)
         return NULL;
@@ -346,33 +304,10 @@ void removeBlock(void *bp){
         NEXT(PREV(bp)) = NEXT(bp);
         PREV(NEXT(bp)) = PREV(bp);
     }
-    place(bp,asize);
-    return bp;
 }
-
-
-void putFreeBlock(void *bp){
-    NEXT(bp) = free_listp;
-    PREV(bp) = NULL;
-    PREV(free_listp) = bp;
-    free_listp = bp;
-}
-
-void removeBlock(void *bp){
-    // last_bp = NEXT(bp);
-    if(bp == free_listp){
-        PREV(NEXT(bp)) = NULL;
-        free_listp = NEXT(bp);
-    }else{
-        NEXT(PREV(bp)) = NEXT(bp);
-        PREV(NEXT(bp)) = PREV(bp);
-    }
-}
-
 /*
  * mm_free - Freeing a block does nothing.
  */
-void mm_free(void *bp)
 void mm_free(void *bp)
 {
     size_t size = GET_SIZE(HDRP(bp));
@@ -388,26 +323,6 @@ void mm_free(void *bp)
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
  */
-void *mm_realloc(void *bp, size_t size){
-    if(size <= 0){ 
-        mm_free(bp);
-        return 0;
-    }
-    if(bp == NULL){
-        return mm_malloc(size); 
-    }
-    void *newp = mm_malloc(size); 
-    if(newp == NULL){
-        return 0;
-    }
-    size_t oldsize = GET_SIZE(HDRP(bp));
-    if(size < oldsize){
-    	oldsize = size; 
-	}
-
-    memcpy(newp, bp, oldsize); 
-    mm_free(bp);
-    return newp;
 void *mm_realloc(void *bp, size_t size){
     if(size <= 0){ 
         mm_free(bp);
