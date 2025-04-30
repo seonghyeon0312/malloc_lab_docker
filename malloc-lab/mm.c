0d140a181ce429ff38c78c1fcdb1768036fea670
usingnameing <cnrrn0312@gmail.com>
2025-04-30 13:35:59 +0900

    Add: macro

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -1,6 +1,7 @@
 /*
  * mm-naive.c - The fastest, least memory-efficient malloc package.
  * 
+ * 
  * In this naive approach, a block is allocated by simply incrementing
  * the brk pointer.  A block is pure payload. There are no headers or
  * footers.  Blocks are never coalesced or reused. Realloc is
@@ -17,6 +18,7 @@
 // @@@@@ explicit @@@@@
 #include <sys/mman.h>
 #include <errno.h>
+#include<math.h>
 
 #include "mm.h"
 #include "memlib.h"
@@ -28,13 +30,18 @@
 team_t team = {
     /* Team name */
     "team 9",
+    "team 9",
     /* First member's full name */
+    "La_Ska",
     "La_Ska",
     /* First member's email address */
     "flaska99@jungle.com",
+    "flaska99@jungle.com",
     /* Second member's full name (leave blank if none) */
-    "msms804",
+    "",
     /* Second member's email address (leave blank if none) */
+    ""
+};
     ""
 };
 /* single word (4) or double word (8) alignment */
@@ -45,6 +52,10 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+// Basic constants and macros
+#define WSIZE 4 // 워드 = 헤더 = 풋터 사이즈(bytes)
+#define DSIZE 8 // 더블워드 사이즈(bytes)
+#define CHUNKSIZE (1<<12) // heap을 이정도 늘린다(bytes)
 // Basic constants and macros
 #define WSIZE 4 // 워드 = 헤더 = 풋터 사이즈(bytes)
 #define DSIZE 8 // 더블워드 사이즈(bytes)
@@ -76,6 +87,22 @@ team_t team = {
 #define PREV(bp) (*(void**)(bp))
 #define NEXT(bp) (*(void**)(bp + WSIZE))
 
+#define log(k) ((unsigned int)(log2f((float)k)))
+#define INDEX(base, k) ((base+(WSIZE*(k-1))))
+
+static void *heap_listp = NULL; // heap 시작주소 pointer
+static void *free_listp = NULL; // free list head - 가용리스트 시작부분
+// static void *last_bp = NULL; // next_fit을 위한 전역변수
+
+static void *coalesce(void *bp);
+static void *extend_heap(size_t words);
+static void *find_fit(size_t asize);
+static void place(void *bp, size_t asize);
+
+void removeBlock(void *bp);
+void putFreeBlock(void *bp);
+
+
 static void *heap_listp = NULL; // heap 시작주소 pointer
 static void *free_listp = NULL; // free list head - 가용리스트 시작부분
 // static void *last_bp = NULL; // next_fit을 위한 전역변수
@@ -93,8 +120,22 @@ int mm_init(void)
 {   
     heap_listp = mem_sbrk(3*DSIZE);
     if (heap_listp == (void*)-1){
+{   
+    heap_listp = mem_sbrk(3*DSIZE);
+    if (heap_listp == (void*)-1){
         return -1;
     }
+    PUT(heap_listp, 0); //Unused padding
+    PUT(heap_listp + WSIZE, PACK(2*DSIZE,1)); 
+    PUT(heap_listp + 2*WSIZE,NULL); 
+    PUT(heap_listp + 3*WSIZE,NULL); 
+    PUT(heap_listp + 4*WSIZE,PACK(2*DSIZE,1)); 
+    PUT(heap_listp + 5*WSIZE,PACK(0,1)); 
+
+    free_listp = heap_listp + DSIZE;
+    // last_bp = free_listp; // next_fit 을 위한 추가
+    // Extend the empty heap with a free block of CHUNKSIZE bytes
+    if (extend_heap(CHUNKSIZE/WSIZE) == NULL) 
     PUT(heap_listp, 0); //Unused padding
     PUT(heap_listp + WSIZE, PACK(2*DSIZE,1)); 
     PUT(heap_listp + 2*WSIZE,NULL); 
@@ -200,7 +241,6 @@ static void *extend_heap(size_t words)
 
 //     return NULL; // 못 찾으면 NULL
 // }
-
 static void *find_fit(size_t asize) {
     void *bp;
     void *best_bp = NULL;
@@ -244,7 +284,12 @@ static void place(void *bp, size_t asize){
 void *mm_malloc(size_t size)
 {
     size_t asize; 
+    size_t asize; 
     size_t extendsize;
+    void *bp; 
+
+   
+    if(size <= 0) 
     void *bp; 
 
    
@@ -260,9 +305,22 @@ void *mm_malloc(size_t size)
    
     if((bp = find_fit(asize))!=NULL){
         place(bp,asize); 
+    
+   
+    if(size <= DSIZE)
+        asize = 2*DSIZE; 
+    else         
+        asize = DSIZE * ((size+(DSIZE)+(DSIZE-1)) / DSIZE);
+
+   
+    if((bp = find_fit(asize))!=NULL){
+        place(bp,asize); 
         return bp;
     }
 
+  
+    extendsize = MAX(asize,CHUNKSIZE);
+    if((bp = extend_heap(extendsize/WSIZE)) == NULL)
   
     extendsize = MAX(asize,CHUNKSIZE);
     if((bp = extend_heap(extendsize/WSIZE)) == NULL)
@@ -292,11 +350,35 @@ void removeBlock(void *bp){
     return bp;
 }
 
+
+void putFreeBlock(void *bp){
+    NEXT(bp) = free_listp;
+    PREV(bp) = NULL;
+    PREV(free_listp) = bp;
+    free_listp = bp;
+}
+
+void removeBlock(void *bp){
+    // last_bp = NEXT(bp);
+    if(bp == free_listp){
+        PREV(NEXT(bp)) = NULL;
+        free_listp = NEXT(bp);
+    }else{
+        NEXT(PREV(bp)) = NEXT(bp);
+        PREV(NEXT(bp)) = PREV(bp);
+    }
+}
+
 /*
  * mm_free - Freeing a block does nothing.
  */
 void mm_free(void *bp)
+void mm_free(void *bp)
 {
+    size_t size = GET_SIZE(HDRP(bp));
+    PUT(HDRP(bp), PACK(size,0));
+    PUT(FTRP(bp), PACK(size,0));
+    coalesce(bp);    
     size_t size = GET_SIZE(HDRP(bp));
     PUT(HDRP(bp), PACK(size,0));
     PUT(FTRP(bp), PACK(size,0));
@@ -306,6 +388,26 @@ void mm_free(void *bp)
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
  */
+void *mm_realloc(void *bp, size_t size){
+    if(size <= 0){ 
+        mm_free(bp);
+        return 0;
+    }
+    if(bp == NULL){
+        return mm_malloc(size); 
+    }
+    void *newp = mm_malloc(size); 
+    if(newp == NULL){
+        return 0;
+    }
+    size_t oldsize = GET_SIZE(HDRP(bp));
+    if(size < oldsize){
+    	oldsize = size; 
+	}
+
+    memcpy(newp, bp, oldsize); 
+    mm_free(bp);
+    return newp;
 void *mm_realloc(void *bp, size_t size){
     if(size <= 0){ 
         mm_free(bp);
